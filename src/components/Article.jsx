b0078b76666b9acfb3c2f425fb4c2ca5b0ebaae6
nomikazu-x <nomikazyu@gmail.com>
2024-05-03 08:55:44 +0900

    useStateのケース

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,16 +1,10 @@
 import {useState} from "react";
-import {Content, Title, PublishButton} from "./index";
+import TextInput from "./TextInput";
 
 const Article = (props) => {
-    const [isPublished, setIsPublished] = useState(false)
-    const publishArticle = () => {
-        setIsPublished(true)
-    }
     return (
         <div>
-           <Title title={props.title} />
-           <Content content={props.content} />
-           <PublishButton isPublished={isPublished} onClick={publishArticle} />
+           <TextInput />
         </div>
     );
 };
