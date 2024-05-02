c16967c810c4c60b538c5916c7ba1dba6beeabe5
nomikazu-x <nomikazyu@gmail.com>
2024-05-03 08:40:53 +0900

    コンポーネントの状態管理

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 export {default as Article} from './Article'
 export {default as Button} from './Button'
 export {default as Content} from './Content'
+export {default as PublishButton} from './PublishButton'
 export {default as Title} from './Title'
