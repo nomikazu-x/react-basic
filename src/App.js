227774b6b1059d52daa593875094fc348bb0ac20
nomikazu-x <nomikazyu@gmail.com>
2024-06-06 21:09:06 +0900

    useState

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import {Article, Title} from "./components/index";
+import {Article, Counter, TextInput, ToggleButton} from "./components";
 
 function App() {
   return (
-    <Article
-      title={'新・日本一わかりやすいReact入門'}
-      content={'今日のトピックはpropsのデータの受け渡しについて'}
-    />
+    // <Article
+    //   title={'新・日本一わかりやすいReact入門'}
+    //   content={'今日のトピックはpropsのデータの受け渡しについて'}
+    // />
+    // <Counter />
+    <ToggleButton />
   );
 }
 
