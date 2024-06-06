fdb59c83a576fcf83f73c45f02f3c50af9d0a071
nomikazu-x <nomikazyu@gmail.com>
2024-06-06 21:51:47 +0900

    useEffect fetch api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import {Article, Counter, TextInput, ToggleButton} from "./components";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [name, setName] = useState('')
+  const [id, setId] = useState('nomikazu-x')
+  const ids = ['nomikazu-x', 'gaearon', 'aws', 'google', 'facebook']
+  const getRondomId = () => {
+    const _id = ids[Math.floor(Math.random() * ids.length)]
+    setId(_id)
+  }
+
+  useEffect(() => {
+    fetch(`https://api.github.com/users/${id}`)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+        setName(data.name)
+      })
+      .catch(error => {
+        console.error(error)
+      })
+  }, [id])
+
   return (
-    // <Article
-    //   title={'新・日本一わかりやすいReact入門'}
-    //   content={'今日のトピックはpropsのデータの受け渡しについて'}
-    // />
-    // <Counter />
-    <ToggleButton />
+    <div>
+      <p>{id}の、GitHubの名前は{name}です。</p>
+      <button onClick={getRondomId}>ID変更</button>
+    </div>
   );
 }
 
